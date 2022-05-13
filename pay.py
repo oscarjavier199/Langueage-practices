eba4309ca7af57c2bc1e14e9f6abb9d82551217f
oscarjavier199 <oscarjavier2@protonmail.com>
2022-05-13 11:24:39 -0500

    python exercises

---
+++ pay.py
@@ -0,0 +1,4 @@
+payI = input("Enter Hours: ")
+payR = input("Enter Rate: ")
+pay = float(payI) * float(payR)
+print("Payment:", pay)
---
+++ usFloor.py
@@ -0,0 +1,5 @@
+# this program will convert elevator floor from Europe to US floor
+
+inp = input('Europe Floor?')
+usf = int(inp) + 1
+print('US Floor:', usf)
