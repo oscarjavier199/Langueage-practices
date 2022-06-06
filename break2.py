e421481c6988ae30bb5f2c805ba99cfa9a1f5695
oscarjavier199 <oscarjavier2@protonmail.com>
2022-06-06 08:43:48 -0500

    python

---
+++ break2.py
@@ -0,0 +1,14 @@
+items_on_sale = ["blue shirt", "striped socks", "knit dress", "red headband", "dinosaur onesie"]
+ 
+print("Checking the sale list!")
+ 
+for item in items_on_sale:
+  print(item)
+  if item == "knit dress":
+    break
+ 
+print("End of search!")
+
+#program will look for a specific item on a list of items
+#then it will break when the item is found and will print
+#end of search
