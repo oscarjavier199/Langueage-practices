fbb853afeed583782a31d67b5af4980a4e35b5f0
oscarjavier199 <oscarjavier2@protonmail.com>
2022-06-14 13:50:27 -0500

    date_time

---
+++ date_time.py
@@ -0,0 +1,7 @@
+from datetime import datetime
+
+date = datetime.now() - datetime(2000, 2, 1)
+print(date)
+
+# program will print how many days, hours, minutes and seconds have passed 
+# from a given date until today
