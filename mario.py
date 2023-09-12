bc2bb4551aadcfbf8fd11631b0733aed7b20bbe8
oscarjavier199 <oscarjavier2@proton.me>
2023-09-12 15:42:19 -0500

    practices in functions, operators and loops

---
+++ mario.py
@@ -0,0 +1,30 @@
+"""""
+def main():
+    print_column(3)
+    
+    
+def print_column(height):
+    for i in range(height):
+        print("#")
+        
+main()
+"""   
+
+"""
+def main():
+    print_row(4)
+
+def print_row(width):
+        print("?" * width)
+        
+main()
+"""
+
+def main():
+    print_square(3)
+
+def print_square(size):
+        for i in range(size):
+            print("#" * size)
+        
+main()
