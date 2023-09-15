2eccb9995b46deea6e749b1df826f03e04a958f6
oscar <oscarjavier2@proton.me>
2023-09-15 16:41:47 -0500

    coke test cs50p

---
+++ cs50p_practices/coke.py
@@ -0,0 +1,10 @@
+user_coins = ["50", "25", "10", "5", "1"] #range 50 to 1
+print("amount due: 50")
+
+
+for coin in user_coins:
+    insert_coin = int(input("insert coin: "))
+ #   new_prices = [insert_coin-5 for coin in user_coins]
+    amound_due = ("amount due: ")
+    print(coin)
+    
