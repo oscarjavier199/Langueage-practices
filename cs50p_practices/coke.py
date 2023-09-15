0029231663e995216ebd7502db716cebb5adbf72
oscar <oscarjavier2@proton.me>
2023-09-15 18:30:41 -0500

    Update coke.py

--- cs50p_practices/coke.py
+++ cs50p_practices/coke.py
@@ -8,3 +8,17 @@ for coin in user_coins:
     amound_due = ("amount due: ")
     print(coin)
     
+   
+   
+
+user_coins = 50 #range 50 to 1
+print("amount due: 50")
+
+
+for coin in range(1, n):
+    insert_coin = int(input("insert coin: "))
+ #   new_prices = [insert_coin-5 for coin in user_coins]
+    total -= int(input("insert coin: "))
+    print(str(total)
+    
+   
