dd51ae0b2d4100f8ff9d326ae605b20dcafa5de1
oscar <oscarjavier2@proton.me>
2023-09-19 18:33:26 -0500

    various examples

---
+++ Python_practices 2.0/fruits.py
@@ -0,0 +1,42 @@
+#simple program with various exercises
+
+#is apple in fruits?
+fruits = ["apple", "banana", "mango", "grapes"]
+if "apple" in fruits:
+    print("apple is a fruit!")
+
+
+#if 5 != to 10:
+if 5 != 10:
+print("5 is not equal to ten :(")
+
+
+
+#if at least one of two statements is True.
+if 10 > 5 or 4 == 4:
+print("At least one statement is True")
+
+
+#Print the second item in the fruits list.
+fruits = ["apple", "banana", "mango", "grapes"]
+print(fruits[1])
+
+
+#Change the value from "apple" to "kiwi", in the fruits list.
+fruits = ["apple", "banana", "mango", "grapes"]
+fruits[0] = "kiwi"
+
+
+#Use the append method to add "orange" to the fruits list.
+fruits = ["apple", "banana", "cherry"]
+fruits.append("orange")
+
+
+#Use the insert method to add "lemon" as the second item in the fruits list.
+fruits = ["apple", "banana", "cherry"]
+fruits.insert(1, "lemon")
+
+#Use the remove method to remove "banana" from the fruits list.
+fruits = ["apple", "banana", "cherry"]
+fruits.remove("banana")
+
