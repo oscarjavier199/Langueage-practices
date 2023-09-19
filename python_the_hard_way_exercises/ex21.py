d57599a3b668f94faefa8182ddfcbf0cb3a8ca6f
oscarjavier199 <oscarjavier2@proton.me>
2023-09-19 14:03:09 -0500

    exercise 21 python the hard way

---
+++ python_the_hard_way_exercises/ex21.py
@@ -0,0 +1,40 @@
+#adding function
+def add(a, b):
+    print(f"ADDING {a} + {b}")
+    return a + b
+
+#subtracting function
+def subtract(a, b):
+    print(f"SUBTRACTING {a} - {b}")
+    return a - b
+
+#multiplying function
+def multiply(a, b):
+    print(f"MULTIPLYING {a} * {b}")
+    return a * b
+
+#dividing function
+def divide(a, b):
+    print(f"DIVIDING {a} / {b}")
+    return a / b
+
+#print
+print("Let's do some math with just functions!")
+
+#here we are calling the functions previously defined and assigning them to some variables
+age= add(30, 5)
+height = subtract(78, 4)
+weight = multiply(90, 2)
+iq = divide(100, 2)
+
+#print message "Age: 35, Height: 74, weight: 180, IQ: 50"
+print("Age: %d, Height: %d, weight: %d, IQ: %d" % (age, height, weight, iq))
+
+#print message "Here is a puzzle:"
+print("Here is a puzzle:")
+
+#divides the iq by 2 then multiplies it by the weight, subtracts tge height and adds the age
+what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
+
+#prints message
+print("That becomes: ", what, "\nCan you do it by hand?")
