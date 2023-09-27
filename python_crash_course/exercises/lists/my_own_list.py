598f1604f7337a972e20ed1be38840dbef7cf020
oscarjavier199 <oscarjavier2@proton.me>
2023-09-27 10:39:56 -0500

    transportation list

---
+++ python_crash_course/exercises/lists/my_own_list.py
@@ -0,0 +1,9 @@
+#program will print messages based on the item on a list
+
+transportation = ['bicycle', 'bus', 'taxi', 'car', 'metro']
+
+print(f'I used to go to school in a red {transportation[0]} every single day!')
+print(f'Sometimes I have to take the {transportation[1]} when I have to go far away in the city.')
+print(f'When I was a teenager my parents gave me money to take a {transportation[2]} everyday after school.')
+print(f"I'd love to own a Porsche {transportation[3]}.")
+print(f'The {transportation[4]} is kinda fast but alway full!!!')
