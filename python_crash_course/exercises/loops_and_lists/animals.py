0a58533f7d00a48f58d16c358971b3e4b1db9c12
oscarjavier199 <oscarjavier2@proton.me>
2023-09-28 10:16:42 -0500

    for loops

---
+++ python_crash_course/exercises/loops_and_lists/animals.py
@@ -0,0 +1,6 @@
+animals = ['dog', 'cat', 'dragon']
+
+for animal in animals:
+    print(f"a {animal.title()} would make a great pet!\n")
+print('''\tEvery single animal in the previous list would make a great pet!
+\t\t\t\tDracarys!''')
---
+++ python_crash_course/exercises/loops_and_lists/pizzas.py
@@ -0,0 +1,8 @@
+pizzas = ['hawaiian', 'pepperoni', 'mushroom chicken']
+print('pizzas:')
+
+for pizza in pizzas:
+    print(f'I really like {pizza} pizza')
+print(f'''\nI really love pizza, not only the previously mentioned
+but all of them, but my favorite will always be...
+pepperoni!''')
