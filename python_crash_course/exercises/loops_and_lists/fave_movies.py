e1965f42b1d2bdb8e83fd08beb78fb929b89d0a2
oscarjavier199 <oscarjavier2@proton.me>
2023-09-28 15:48:49 -0500

    copying lists and slicing

---
+++ python_crash_course/exercises/loops_and_lists/fave_movies.py
@@ -0,0 +1,17 @@
+#program will use for loops, list slicing, append() and pop()
+# to print a message
+
+my_movies = ['harry potter', 'the lord of the rings', 'sicario', 'narnia']
+friends_movies = my_movies[:]
+
+print('\nmy favorite movies are:')
+for my_movie in my_movies:
+    print('*', my_movie.title())
+    
+friends_movies.append('scary movie')
+friends_movies.pop(0)
+friends_movies.pop(3)
+    
+print('\nMy friend\'s favorite movies are:')
+for friend_movie in friends_movies:
+    print('*', friend_movie)
