db4b543f88df86e3dc610f835e50474d56cce5ef
oscarjavier199 <oscarjavier2@proton.me>
2023-09-29 08:03:54 -0500

    tuples

--- python_crash_course/exercises/loops_and_lists/buffet_tuples.py
+++ python_crash_course/exercises/loops_and_lists/buffet_tuples.py
@@ -1,1 +1,13 @@
-foods = ('pasta', 'beans', 'salad', 'lentils', 'chicken rice')
+#assigning new values to a tuple
+
+#tuple:
+foods = ('pasta', 'beans', 'salad', 'lentils', 'chicken breast')
+
+print('old menu:')
+for food in foods: print(food)
+
+foods = ('lasagna', 'beans', 'salad', 'steak', 'chicken breast')
+print ('\nnew menu:')
+for food in foods: print(food)
+
+
