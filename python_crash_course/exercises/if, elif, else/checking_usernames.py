4fcbbf8352f1e726b751c35c6b18a13fe9b7a82f
oscarjavier199 <oscarjavier2@proton.me>
2023-09-30 09:50:17 -0500

    changing chacking_usernames to more readable code

--- python_crash_course/exercises/if, elif, else/checking_usernames.py
+++ python_crash_course/exercises/if, elif, else/checking_usernames.py
@@ -1,25 +1,18 @@
+#program will take a couple of lists containing usernames, if a username from 
+# new usernames is in the list of existing users it will inform that to the user
 
 current_users = ['OSCAR999', 'DR.HOUSE1', 'DRACULA111', 'BATMAN05',
 'HARLEY_LOVES_JOKER', 'ADMIN']
 
-#copy of list of usernames and appends of lower case usernames
-current_users_lower = current_users[:]
-current_users_lower.append('oscar999')
-current_users_lower.append('dr.house1')
-current_users_lower.append('dracula111')
-current_users_lower.append('harley_loves_joker')
-current_users_lower.append('admin')
+current_users_lower = ['oscar999', 'dr.house1', 'dracula111', 
+'harley_loves_joker', 'admin']
 
 new_users = ['OSCAR999', 'DR.HOUSE1', 'superm4n', 'hulk_smash',
 'resident_of_evil4']
 
 for new_user in new_users:
-    if new_user in current_users:
+    if new_user in current_users or new_user in current_users_lower:
         print(f'''Sorry, you need to enter a new user name, {new_user} is 
 already taken 🙃''')
     else:
         print(f"Congrats! {new_user} is available 🥳")
-        
-        
-
-        
