879492406323c456fd59bb69adaf9eb1e60edc6e
oscarjavier199 <oscarjavier2@proton.me>
2023-10-02 09:34:13 -0500

    dict of numbers

---
+++ python_crash_course/exercises/dictionaries/favorite_numbers.py
@@ -0,0 +1,15 @@
+#program will access the values of some keys
+
+favorite_numbers = {
+    'charlie': 3, 
+    'michael': 55,
+    'yolanda': 777,
+    'rick': 1, 
+    'walter': 4,
+}
+
+print(f"Charlie's favorite number is: {favorite_numbers['charlie']} 🙃")
+print(f"michael's favorite number is: {favorite_numbers['michael']} 🤪")
+print(f"Yolanda's favorite number is {favorite_numbers['yolanda']} 🤔")
+print(f"rick's favorite number is: {favorite_numbers['rick']} 🤐")
+print(f"Walter's favorite number is {favorite_numbers['walter']} 😶")
