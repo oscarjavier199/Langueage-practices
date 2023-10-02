9a44a8371baeb997322464e7f72d7ab241536a9a
oscarjavier199 <oscarjavier2@proton.me>
2023-10-02 09:49:35 -0500

    glossary

---
+++ python_crash_course/exercises/dictionaries/glossary.py
@@ -0,0 +1,21 @@
+#program will print some words and their meaning using a dictionary
+
+glossary = {
+    'python': 'programming language',
+    'dictionary': 'connects pieces of related data',
+    'lists': 'stores multiple items in a single variable',
+    'int': 'integer number (1)',
+    'float': 'floating number (1.0)',
+}
+
+python = glossary['python']
+dictionary = glossary['dictionary']
+list = glossary['lists']
+int = glossary['int']
+float = glossary['float']
+
+print(f"Python:\n{python}\n")
+print(f"Dictionary:\n{dictionary}\n")
+print(f"List:\n{list}\n")
+print(f"Int:\n{int}\n")
+print(f"Float:\n{float}")
