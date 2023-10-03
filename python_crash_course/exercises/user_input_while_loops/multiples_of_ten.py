e944f3677bef732cc9a540f90c16027c81622d03
oscarjavier199 <oscarjavier2@proton.me>
2023-10-03 06:57:28 -0500

    inputs and modulo

---
+++ python_crash_course/exercises/user_input_while_loops/multiples_of_ten.py
@@ -0,0 +1,7 @@
+number = input("Please enter a number >>> ")
+number = int(number)
+
+if number % 10 == 0:
+    print(f"{number} is Multiple of ten! 😁")
+else:
+    print(f"{number} is not multiple of ten 😪")
