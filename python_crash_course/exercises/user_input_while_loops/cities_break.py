bbf55348e97503ef464e8d328514f18f39ee1a87
oscar <oscarjavier2@proton.me>
2023-10-03 10:10:37 -0500

    break statement

---
+++ python_crash_course/exercises/user_input_while_loops/cities_break.py
@@ -0,0 +1,9 @@
+prompt = "\nPlease enter the name of a city you have visited: "
+prompt += "\nEnter 'quit' qhen you are finished >>> "
+
+while True:
+    city = input(prompt)
+    if city == 'quit':
+        break
+    else:
+        print(f"I'd love to go to {city.title()}!")
