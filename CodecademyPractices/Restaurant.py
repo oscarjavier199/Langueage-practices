00647b33e47525e5565b86fd760d8984e3492b45
oscar <98227360+oscarjavier199@users.noreply.github.com>
2023-10-30 06:52:06 -0500

    class inheritance practice

--- CodecademyPractices/Restaurant.py
+++ CodecademyPractices/Restaurant.py
@@ -2,17 +2,36 @@ class Restaurant:
     def __init__(self, restaurant_name, cuisine_type):
         self.restaurant_name = restaurant_name
         self.cuisine_type = cuisine_type
+        self.number_served = number_served = 0
 
     def describe_restaurant(self):
         print(f"{self.restaurant_name}, {self.cuisine_type}")
 
     def open_restaurant(self):
-        print("Restaurant is open for business!")
+        print("Restaurant is open for business!\n")
+
+    def num_customers(self, number_served):
+        return self.number_served + number_served
+
+
+class IceCreamStand(Restaurant):
+    def __init__(self, restaurant_name, cuisine_type):
+        super().__init__(restaurant_name, cuisine_type)
+        self.flavors = ['Vanilla', 'chocolate', 'cookies and cream', 'brownie']
+
+    def open_restaurant(self):
+        print("Restaurant will be open for business after 4:00 pm!")
+
+    def ice_flavors(self):
+        print("Ice Cream Flavors: ")
+        for flavor in self.flavors:
+            print(f"-{flavor}")
 
 
 restaurant = Restaurant("rick's lasagna", "Italian cuisine")
 restaurant.describe_restaurant()
 restaurant.open_restaurant()
+print(f"Customers served: {restaurant.num_customers(45)}\n")
 
 restaurant_2_mex = Restaurant("mexictacos", "Mexican food")
 restaurant_2_mex.describe_restaurant()
@@ -22,3 +41,7 @@ restaurant_3_fast_food = Restaurant("fasty", "Fast Food")
 restaurant_3_fast_food.describe_restaurant()
 restaurant_3_fast_food.open_restaurant()
 
+ice_cream_shop = IceCreamStand("Cookies & cream", "Ice creams and desserts")
+ice_cream_shop.describe_restaurant()
+ice_cream_shop.open_restaurant()
+ice_cream_shop.ice_flavors()
--- CodecademyPractices/user.py
+++ CodecademyPractices/user.py
@@ -4,6 +4,7 @@ class User:
         self.last_name = last_name
         self.member_since = member_since
         self.username = username
+        self.login_attempts = 0
 
     def describe_user(self):
         print(
@@ -12,7 +13,16 @@ class User:
     def greet_user(self):
         print(f"\nWelcome back {self.username}!")
 
+    def increment_login_attempts(self):
+        self.login_attempts += 1
+        return self.login_attempts
+
+    def reset_login_attempts(self):
+        self.login_attempts = 0
+        return self.login_attempts
+
 
 user_1 = User("oscar", "perez", 2012, "oscarjvz100")
 user_1.describe_user()
 user_1.greet_user()
+print(f"Logging Attempts: {user_1.increment_login_attempts()}")
