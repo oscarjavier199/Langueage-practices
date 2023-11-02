bb5b0db5feb060b757dedc3d7b3a1ed695af8dc1
oscar <98227360+oscarjavier199@users.noreply.github.com>
2023-11-02 08:34:44 -0500

    classes in python practice

--- Codecademy_python_practices/class_employee.py
+++ Codecademy_python_practices/class_employee.py
@@ -9,8 +9,16 @@ class Employee:
         print(f"My ID is {self.id}")
 
 
+class Admin(Employee):
+    def say_id(self):
+        print("I am an admin!")
+
+
 e1 = Employee()
 e1.say_id()
 
 e2 = Employee()
 e2.say_id()
+
+e3 = Admin()
+e3.say_id()
